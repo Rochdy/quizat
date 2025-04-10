5b604856bd07b8d598173257d06b576eb2f8a09e
Mohammed Roshdy <mohammed.roshdy@cobrowser.net>
2025-04-10 22:08:33 +0200

    fix: admin

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//})->name('home');
+Route::get('/', function () {
+    return redirect('/admin');
+})->name('home');
 //
 //Route::view('dashboard', 'dashboard')
 //    ->middleware(['auth', 'verified'])
